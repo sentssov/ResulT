093d3953e0d183308f68dd356951ffdbdb609643
Alexander Sentsov <alexander.sentsov03@gmail.com>
2023-10-15 21:12:18 +0500

    (Feature) Add Result.FoldTry method for IEnumerable<T>

--- ResulT.Extensions/Fold.cs
+++ ResulT.Extensions/Fold.cs
@@ -49,6 +49,59 @@ public static partial class ResultExtensions
         return await result.Fold(seed, func);
     }
 
+    public static Result<TIn> FoldTry<TSrc, TIn>(this Result<TSrc> result,
+        TIn seed, Func<TIn, TIn?, TIn> func, Func<Exception, Error> expHandler) 
+        where TSrc : IEnumerable<TIn>
+    {
+        if (result.IsFailure)
+            return Result.Failure<TIn>(result.Errors);
+
+        return Result.Try(() =>
+        {
+            TIn output = seed;
+            foreach (var element in result.Value!)
+            {
+                output = func(output, element);
+            }
+            return output;
+        }, expHandler);
+    }
+    public static async Task<Result<TIn>> FoldTry<TSrc, TIn>(this Task<Result<TSrc>> resultTask,
+        TIn seed, Func<TIn, TIn?, TIn> func, Func<Exception, Error> expHandler)
+        where TSrc : IEnumerable<TIn>
+    {
+        var result = await resultTask
+            .ConfigureAwait(false);
+
+        return result.FoldTry(seed, func, expHandler);
+    }
+    public static async Task<Result<TIn>> FoldTry<TSrc, TIn>(this Result<TSrc> result,
+        TIn seed, Func<TIn, TIn?, Task<TIn>> func, Func<Exception, Error> expHandler)
+        where TSrc : IEnumerable<TIn>
+    {
+        if (result.IsFailure)
+            return Result.Failure<TIn>(result.Errors);
+
+        return await Result.Try(async () =>
+        {
+            TIn output = seed;
+            foreach (var element in result.Value!)
+            {
+                output = await func(output, element);
+            }
+            return output;
+        }, expHandler);
+    }
+    public static async Task<Result<TIn>> FoldTry<TSrc, TIn>(this Task<Result<TSrc>> resultTask,
+        TIn seed, Func<TIn, TIn?, Task<TIn>> func, Func<Exception, Error> expHandler) 
+        where TSrc : IEnumerable<TIn>
+    {
+        var result = await resultTask
+            .ConfigureAwait(false);
+
+        return await result.FoldTry(seed, func, expHandler);
+    }
+    
     public static Result<TIn> FoldIf<TSrc, TIn>(this Result<TSrc> result,
         bool condition, TIn seed, Func<TIn, TIn?, TIn> func, Error error) 
         where TSrc : IEnumerable<TIn>
